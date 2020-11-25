e2f7c31137700e878d451c35e9b4c19fa3d4162a
Adriano Dias <adriano.rj.br@gmail.com>
2020-11-25 09:16:25 -0300

    first commit

---
+++ small-shopping-cart/src/App.js
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Basket from "./components/Basket";
+import data from "./data";
+
+function App() {
+  const { products } = data;
+  return (
+    <div>
+      <Header></Header>
+      <div className="row">
+        <Main products={products}></Main>
+        <Basket></Basket>
+      </div>
+    </div>
+  );
+}
+
+export default App;
